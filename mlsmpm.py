2730c68ddd09359e3ea9826ea4ea5db78e0d001c
Fumiya Fujinaka <uzuna.kf@gmail.com>
2025-02-05 22:11:06 +0900

    fix: パーティクル表示demo

--- mlsmpm.py
+++ mlsmpm.py
@@ -29,25 +29,37 @@ class StaticBall:
 
 
 def main():
-    window = ti.ui.Window("Taichi Cloth Simulation on GGUI", (1024, 1024), vsync=True)
+    # 質点の初期化。XYZ方向に伸ばしている
+    @ti.kernel
+    def init_points_pos(points: ti.template()):
+        for i in range(points.shape[0]):
+            points[i] = [i * 0.1 for j in ti.static(range(3))]
+
+    window = ti.ui.Window("Taichi Particle view", (1024, 1024), vsync=True)
 
     canvas = window.get_canvas()
     canvas.set_background_color((1, 1, 1))
     scene = ti.ui.Scene()
     camera = ti.ui.Camera()
 
-    # ボールの初期設定
-    ball = StaticBall(0.3, [0.0, 0.0, 0.0])
+    # シーンの設定。カメラの位置、ライトの設定、メッシュの描画
+    camera.position(5, 2, 2)
+    camera.lookat(0.0, 0.0, 0)
+
+    # パーティクル作成
+    N = 50
+    particles_pos = ti.Vector.field(3, dtype=ti.f32, shape=N)
+    init_points_pos(particles_pos)
 
     while window.running:
-        # シーンの設定。カメラの位置、ライトの設定、メッシュの描画
-        camera.position(0.0, 0.0, 3)
-        camera.lookat(0.0, 0.0, 0)
+        # キーボードによる移動に対応
+        camera.track_user_inputs(window, movement_speed=0.03, hold_key=ti.ui.RMB)
         scene.set_camera(camera)
 
-        scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
-        scene.ambient_light((0.5, 0.5, 0.5))
-        ball.draw(scene)
+        scene.ambient_light((0.8, 0.8, 0.8))
+        scene.point_light(pos=(0.5, 1.5, 1.5), color=(1, 1, 1))
+
+        scene.particles(particles_pos, color=(0.68, 0.26, 0.19), radius=0.02)
         canvas.scene(scene)
         window.show()
 
